8603b51e05dacd74f6294e4c875498b41a44af92
Wail <wailay>
2021-09-30 23:29:54 -0400

    identify command

--- client/src/app/sidebar/sidebar.component.ts
+++ client/src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { CommunicationService, DroneType } from 'src/services/communication/communication.service';
+import { CommunicationService, Drone, DroneType } from 'src/services/communication/communication.service';
+
+export type DroneRegistry = { [key in DroneType]: Drone[] };
 
 @Component({
     selector: 'app-sidebar',
@@ -8,6 +10,8 @@ import { CommunicationService, DroneType } from 'src/services/communication/comm
 })
 export class SidebarComponent implements OnInit {
     droneType: DroneType;
+    droneRegistry: DroneRegistry = { CRAZYFLIE: [], ARGOS: [] };
+    selectedCraziflyDrone!: string;
 
     constructor(public communicationService: CommunicationService) {
         this.droneType = 'CRAZYFLIE';
@@ -16,15 +20,29 @@ export class SidebarComponent implements OnInit {
     ngOnInit(): void {}
 
     startMission(): void {
-        this.communicationService.startMission();
+        this.communicationService.startMission().subscribe((drones: Drone[]) => {
+            this.droneRegistry[this.communicationService.droneType] = drones;
+        });
     }
 
     endMission(): void {
-        this.communicationService.endMission();
+        this.communicationService.endMission().subscribe((drones: Drone[]) => {
+            this.droneRegistry[this.communicationService.droneType] = drones;
+        });
     }
 
     returnToBase(): void {
-        this.communicationService.returnToBase();
+        this.communicationService.returnToBase().subscribe((drones: Drone[]) => {
+            this.droneRegistry[this.communicationService.droneType] = drones;
+        });
+    }
+
+    identifyDrone(uuid: string): void {
+        console.log('ident', uuid);
+        if (!uuid) return;
+        this.communicationService.identifyDrone(uuid).subscribe((drone: Drone) => {
+            console.log('Identified drone success ', drone);
+        });
     }
 
     setDroneType(type: DroneType): void {
--- client/src/services/communication/communication.service.ts
+++ client/src/services/communication/communication.service.ts
@@ -4,24 +4,34 @@ import { Observable } from 'rxjs';
 
 export type DroneType = 'ARGOS' | 'CRAZYFLIE';
 
+export interface Drone {
+    uuid: string;
+    state: string;
+    type: DroneType;
+}
+
 @Injectable({
     providedIn: 'root',
 })
 export class CommunicationService {
-    base_url: string = 'http://0.0.0.0:8000';
+    base_url: string = 'http://localhost:8000';
     droneType: DroneType = 'CRAZYFLIE';
 
     constructor(private http: HttpClient) {}
 
-    startMission(): Observable<void> {
+    startMission(): Observable<Drone[]> {
         console.log(this.droneType);
-        return this.http.get<void>(`${this.base_url}/start_mission?drone_type${this.droneType}`);
+        return this.http.post<Drone[]>(`${this.base_url}/start_mission?drone_type=${this.droneType}`, {});
     }
-    endMission(): Observable<void> {
-        return this.http.get<void>(`${this.base_url}/end_mission?drone_type${this.droneType}`);
+    endMission(): Observable<Drone[]> {
+        return this.http.post<Drone[]>(`${this.base_url}/end_mission?drone_type=${this.droneType}`, {});
     }
-    returnToBase(): Observable<void> {
-        return this.http.get<void>(`${this.base_url}/return_to_base?drone_type${this.droneType}`);
+    returnToBase(): Observable<Drone[]> {
+        return this.http.post<Drone[]>(`${this.base_url}/return_to_base?drone_type=${this.droneType}`, {});
+    }
+
+    identifyDrone(uuid: string): Observable<Drone> {
+        return this.http.post<Drone>(`${this.base_url}/crazyflie/identify?uuid=${uuid}`, {});
     }
 
     setDroneType(type: DroneType) {
