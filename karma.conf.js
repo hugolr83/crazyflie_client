c66779e4229e757e7e71041ee7a495e9600788db
William Trépanier <william.trepanier@polymtl.ca>
2021-10-03 20:17:24 -0400

    chore: Yet another try

--- karma.conf.js
+++ karma.conf.js
@@ -33,8 +33,14 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome', 'ChromeHeadless'],
+        browsers: ['Chrome', 'ChromeHeadlessNoSandbox'],
         singleRun: false,
         restartOnFileChange: true,
+        customLaunchers: {
+            ChromeHeadlessNoSandbox: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox'],
+            },
+        },
     });
 };
