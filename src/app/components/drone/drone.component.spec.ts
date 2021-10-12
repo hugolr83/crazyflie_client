041eda2b8460b009687fdeba791523d7527eb635
Lachieze-Rey Hugo <hugo.lachieze-rey@polymtl.ca>
2021-10-12 09:45:01 -0400

    fix lint and test

--- src/app/components/drone/drone.component.spec.ts
+++ src/app/components/drone/drone.component.spec.ts
@@ -1,28 +1,26 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DroneComponent } from './drone.component';
 
 describe('DroneComponent', () => {
-  let component: DroneComponent;
-  let fixture: ComponentFixture<DroneComponent>;
+    let component: DroneComponent;
+    let fixture: ComponentFixture<DroneComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DroneComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            declarations: [DroneComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DroneComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DroneComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
