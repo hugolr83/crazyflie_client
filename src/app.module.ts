ef26c0dfc854c4e05e29aa0a75e79d7f73bae021
Hugo Lachieze-Rey <hugo.lachieze-rey@polymtl.ca>
2021-12-02 22:05:37 +0000

    feat/inputs for positions and orientation

--- src/app.module.ts
+++ src/app.module.ts
@@ -37,7 +37,7 @@ import { HistoryComponent } from './app/components/dialog-boxes/history/history.
 import { InformationComponent } from './app/components/dialog-boxes/information/information.component';
 import { SidebarComponent } from './app/components/sidebar/sidebar.component';
 import { TopNavComponent } from './app/components/top-nav/top-nav.component';
-import { HelpComponent } from './app/components/dialog-boxes/help/help.component';
+import { DronePulseComponent } from './app/components/dashboard/drone-container/drone/drone-pulse/drone-pulse.component';
 registerLocaleData(en);
 
 @NgModule({
@@ -54,7 +54,7 @@ registerLocaleData(en);
         PercentageBarComponent,
         InformationComponent,
         HistoryComponent,
-        HelpComponent,
+        DronePulseComponent,
     ],
     imports: [
         HttpClientModule,
