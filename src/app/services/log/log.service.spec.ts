98dc807dcb614d48d822f27b2cdea764ff4ebb10
Lachieze-Rey Hugo <hugo.lachieze-rey@polymtl.ca>
2021-12-05 21:40:24 -0500

    fix format time test

--- src/app/services/log/log.service.spec.ts
+++ src/app/services/log/log.service.spec.ts
@@ -6,8 +6,8 @@ describe('LogService', () => {
     let service: LogService;
     let stamp: MissionTimestamp = {
         date: '2021-12-5',
-        time: '14:19:24',
-        date_time: '[2021-12-5]        14:19:24        ',
+        time: '09:19:24',
+        date_time: '[2021-12-5]        09:19:24        ',
     };
 
     beforeEach(() => {
@@ -26,7 +26,8 @@ describe('LogService', () => {
     });
 
     it('should format time', () => {
-        let timestamp: string = 'Sun, 05 Dec 2021 19:19:24 GMT';
+        let date: Date = new Date('Sun, 05 Dec 2021 14:19:24 UTC');
+        let timestamp: string = date.toUTCString();
         service.formatTimestamp(timestamp);
         expect(service.formatTimestamp(timestamp)).toEqual(stamp);
     });
