0b87ed32899b42ef668ee1367c307706ef7d56ea
sami <sami.sadfa@polymtl.ca>
2021-12-08 01:05:04 -0500

    comm finish

--- src/app/services/communication/communication.service.spec.ts
+++ src/app/services/communication/communication.service.spec.ts
@@ -12,11 +12,28 @@ describe('Service: communication', () => {
             imports: [HttpClientTestingModule],
             providers: [],
         });
-
+        jasmine.clock().install();
+        
         communicationService = TestBed.inject(CommunicationService);
+    });
+
+    afterEach(() => {
+        jasmine.clock().uninstall();
     });
 
     it('should create', () => {
         expect(communicationService).toBeTruthy();
+    });
+
+    it('listenDronePulse should listen to pulse of drone', () => {
+        let drones: any[] = [];
+
+        communicationService.listenDronePulse().subscribe((dronesP) => {
+            drones=dronesP;
+        })
+
+        jasmine.clock().tick(1001);
+
+        expect(drones.length).toBe(0);
     });
 });
